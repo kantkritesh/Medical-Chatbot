619b228e1805cbc345853a26f1204dd8dbda56ca
kantkritesh <kriteshkant5@gmail.com>
2026-01-17 23:28:58 +0530

    folder struture added

---
+++ templates.sh
@@ -0,0 +1,15 @@
+#creating directory
+
+mkdir -p src
+mkdir -p research
+
+#Creating files
+
+touch src/__init__.py
+touch src/helper__init__.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt  
+
